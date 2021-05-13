41ab76524f41ac80c361edfe3c3fcc04a1f68865
Me <yxhuawei@qq.com>
2021-05-13 17:13:17 +0800

    增加了菜单处理部分

--- anyi.js
+++ anyi.js
@@ -63,11 +63,13 @@ void function($) {
      */
     ANYI.prototype.get =function(u,d,f) {
         var o = new XMLHttpRequest();
-        o.open('GET', u);
-        o.send();
+        //待处理拼接参数
+        o.open('GET', u, true);
+		o.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        o.send(null);
         o.onreadystatechange = function () {
             if (o.readyState == 4) {
-                f(o.responseText,o.status);
+                f(o.responseText, o.status);
             }
         }
     }
@@ -121,6 +123,84 @@ void function($) {
         var reg = eval("/(?:^|;\\s*)" + n + "=([^=]+)(?:;|$)/");
         return  decodeURIComponent(reg.test(doc.cookie) ? RegExp.$1 : "");
     }
+
+
+    /** 提供对菜单的初始化和添加 */
+    ANYI.prototype.menus={};
+    
+    /**
+     * 向集合中添加菜单元素(待增加递归处理)
+     * @param {*} id 菜单的id
+     * @param {*} o 菜单参数
+     */
+    ANYI.prototype.addmenu= function (id,o) {
+        var m = document.getElementById(id);//获取元素
+        var title = document.createElement("span");
+        title.innerHTML = o["title"];
+        m.appendChild(title);//设置菜单标题
+        m.setAttribute("anyi-expand", "true");//设置默认为展开
+        //为菜单关联展开函数
+        m.onclick=function(e){
+            e.target.setAttribute("anyi-expand",e.target.getAttribute("anyi-expand")=="false"?"true":"false");
+        };
+        //循环添加元素
+        if(o["items"]){
+            var ul = document.createElement("ul");
+            m.appendChild(ul);
+            for (let index = 0; index < o["items"].length; index++) {
+                const el = o["items"][index];
+                var li = document.createElement("li");
+                ul.appendChild(li);
+                if(el["url"]){
+                    li.innerHTML = "<a href='"+ el["url"] +"'>" + el["title"] + "</a>";//为菜单追加原始url
+                }else {
+                    li.innerText = el["title"];
+                    if(el["anyiurl"]){
+                        li.setAttribute("anyi-url", el["anyiurl"]);
+                        li.onclick=function(e) {
+                            clickurl(e.target.getAttribute("anyi-url"));//子页面的url处理事件
+                        }
+                    };
+                };
+                if(el["onclick"])  li.onclick=el["onclick"];//如果有自定义事件则覆盖
+            }
+        }
+        //ANYI.menus[id]=o;//存储配置
+    }
+
+    /**
+     * 对菜单进行初始化(待增加递归处理)
+     * @param {*} id 菜单id
+     */
+    ANYI.prototype.intimenu=function(id) {
+        var m = document.getElementById(id);//获取元素
+        for (let index = 0; index < m.childNodes.length; index++) {
+            const el = m.childNodes[index];
+            
+        }
+    }
+
+
+
+    /** html页面中的子页面 */
+    ANYI.prototype.content;
+    /**
+     * 在子页面中打开链接
+     * @param {*} url 要打开的url
+     */
+    var clickurl = function(url) {
+        ANYI.prototype.get(url, null, function(d) {
+            ANYI.prototype.content.innerHTML = d;//待调试
+        });
+    }
+    /**
+     * 为页面设置子页面
+     * @param {*} id 子页面的divID
+     */
+    ANYI.prototype.setcontent = function(id) {
+        ANYI.content = document.getElementById(id);//获取元素
+    }
+    
 
 
 
